2324692988aaae664a745ac972b9ae6c4f404ac8
imodre <btima@mail.ru>
2023-09-24 16:19:33 +0300

    Refactor IntMatrix backing structure to flat array
    Changed the backingMatrix in IntMatrix.kt from a 2D array to a single 1D array. This modification was done to improve space efficiency and read/write operations. Instead of using arrays of arrays we are now using the concept of a 'flat' array, saving space in memory and improving the speed of access and alteration.

--- practice/matrix/src/IntMatrix.kt
+++ practice/matrix/src/IntMatrix.kt
@@ -3,20 +3,20 @@ class IntMatrix(val rows: Int, val columns: Int) {
         require(rows >= 0 && columns >= 0) { "Arrays with negative size are unavailable yet :`(" }
     }
 
-    private var backingMatrix: Array<IntArray> = Array(rows) { IntArray(columns) }
+    private var backingMatrix: IntArray = IntArray(rows * columns)
 
     constructor(rows: Int, columns: Int, initializer: (Int, Int) -> Int) : this(rows, columns) {
-        backingMatrix = Array(rows) { row -> IntArray(columns) { column -> initializer(row, column) } }
+        backingMatrix = IntArray(rows * columns) { initializer(it / columns, it % columns) }
     }
 
     operator fun get(i: Int, j: Int): Int {
         validate(i, j)
-        return backingMatrix[i][j]
+        return backingMatrix[i * columns + j]
     }
 
     operator fun set(i: Int, j: Int, value: Int) {
         validate(i, j)
-        backingMatrix[i][j] = value
+        backingMatrix[i * columns + j] = value
     }
 
     private fun validate(i: Int, j: Int) {
